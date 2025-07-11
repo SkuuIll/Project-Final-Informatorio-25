e53c88f6bdf9daf000adf09c6bfc3b0a383b7c7c
SkuuIll <moreappmix@gmail.com>
2025-07-12 02:09:43 +0800

    Fix: Actualiza dependencias y corrige configuración de apps

--- blog/configuraciones/settings.py
+++ blog/configuraciones/settings.py
@@ -21,6 +21,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
 
     # Apps de terceros
     'django_ckeditor_5',
