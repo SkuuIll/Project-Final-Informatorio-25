51810391b0d5a6f6f30c4cbabba4253fd6e05729
Skull <moreappmix@gmail.com>
2025-07-14 01:17:48 +0800

    Update

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,5 +9,8 @@ done
 # Ejecutar las migraciones de la base de datos
 python manage.py migrate
 
+# Recopilar archivos estáticos
+python manage.py collectstatic --noinput
+
 # Iniciar el servidor de producción con Gunicorn
 exec gunicorn blog.wsgi:application --bind 0.0.0.0:8000 --workers 3
