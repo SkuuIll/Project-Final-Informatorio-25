6146fa69b33b8fc347e88d98b8675b689d2650a1
Skuuill <moreappmix@gmail.com>
2025-07-18 15:31:04 -0300

    update

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,36 @@
 from django.contrib import admin
-from .models import Profile
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+from .models import Profile, Notification
+
+# Define un inline para el modelo Profile
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'Perfil'
+    fields = ('avatar', 'bio', 'can_post')
+
+# Define un nuevo UserAdmin
+class UserAdmin(BaseUserAdmin):
+    inlines = (ProfileInline,)
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'get_can_post')
+    list_select_related = ('profile',)
+
+    def get_can_post(self, instance):
+        return instance.profile.can_post
+    get_can_post.short_description = 'Puede postear'
+    get_can_post.boolean = True
+
+# Re-registra el User admin
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+
+# Registra el modelo Notification
+@admin.register(Notification)
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ('recipient', 'sender', 'message', 'is_read', 'created_at')
+    list_filter = ('is_read', 'created_at')
+    search_fields = ('recipient__username', 'sender__username', 'message')
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
