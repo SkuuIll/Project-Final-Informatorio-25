a21e0080521aede0b767aa8214a6c236c9337c98
SkuuIll <moreappmix@gmail.com>
2025-07-26 02:31:41 -0300

    Update

--- blog/configuraciones/settings.py
+++ blog/configuraciones/settings.py
@@ -253,9 +253,6 @@ CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.redis.RedisCache',
         'LOCATION': os.environ.get('REDIS_URL', 'redis://127.0.0.1:6379/1'),
-        'OPTIONS': {
-            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
-        },
         'KEY_PREFIX': 'devblog',
         'TIMEOUT': 300,
     }
